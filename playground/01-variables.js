2b855aa86e7530edba40fef62d7fa3bad14e8cb3
Yaroslav Nikonov <ukimbo357@gmail.com>
2026-05-20 15:31:55 +0300

    Добавил метод join

--- playground/01-variables.js
+++ playground/01-variables.js
@@ -5,4 +5,5 @@ const hobbies = [
     "Баскетбол", 
     "Видеоигры",
     "Футболл"]
-console.log ( `Меня зовут ${name}, мне ${age} лет. Я студент: ${isStudent}. Хобби: ${hobbies}.` )
+
+console.log ( `Меня зовут ${name}, мне ${age} лет. Я студент: ${isStudent}. Хобби: ${hobbies.join(", ")}.` )
