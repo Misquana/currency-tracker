570d987d83324ab13f84ee6e10c4da5008f7153b
Yaroslav Nikonov <ukimbo357@gmail.com>
2026-05-20 20:41:48 +0300

    feat(playground): Fixed margins again

--- playground/02-functions.js
+++ playground/02-functions.js
@@ -1,37 +1,38 @@
 function greet(name){
-    if (name) {
+  if (name) {
     return `Привет, ${name}!`;
   } else { 
     return "Привет, незнакомец!";
-    }
+  }
 }
 
 
 function sum(a,b) {
     return a + b;
 }
+
 const result = sum(3, 4);
 
 
 
 
 const isAdult = (age) => {
-if (age >=18) {
+  if (age >=18) {
     return true;      
   } else {
     return false;
-    }
+  }
 }
 
 
 
 const getMaxOfThree = (a, b, c) => {
-    if (a > b && a > c) {
+  if (a > b && a > c) {
     return a;
   } else if (a < b && c < b) {
     return b;
   } else {
     if (a < c && c > b)
-    return c
-    }
+      return c
+  }
 }  
