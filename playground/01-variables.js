47c57db3c1163813831e3cf8484f64c912a0882c
Yaroslav Nikonov <ukimbo357@gmail.com>
2026-05-20 14:55:10 +0300

    feat(playground): add variables exercise

---
+++ playground/01-variables.js
@@ -0,0 +1,8 @@
+const name = "Ярик"
+const age = 18
+const isStudent = true
+const hobbies = [
+    "Баскетбол", 
+    "Видеоигры",
+    "Футболл"]
+console.log ( `Меня зовут ${name}, мне ${age} лет. Я студент: ${isStudent}. Хобби: ${hobbies}.` )
