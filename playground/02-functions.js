21e2e2907a84200817e10d99230e6b2f67fa4363
Yaroslav Nikonov <ukimbo357@gmail.com>
2026-05-20 18:49:28 +0300

    feat(playground): add functions exercise

---
+++ playground/02-functions.js
@@ -0,0 +1,37 @@
+function greet(name){
+if (name) {
+return `Привет, ${name}!`;
+} else { 
+return "Привет, незнакомец!";
+}
+}
+
+
+function sum(a,b) {
+   return a + b;
+}
+const result = sum(3, 4)
+
+
+
+
+const isAdult = (age) => {
+    if (age >=18) {
+    return true;      
+    } else {
+    return false;
+    }
+}
+
+
+
+const getMaxOfThree = (a, b, c) => {
+    if (a > b && a > c) {
+    return a;
+    } else if (a < b && c < b) {
+    return b;
+    } else {
+    if (a < c && c > b)
+    return c
+    }
+    }  
