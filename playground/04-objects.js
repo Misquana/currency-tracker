64e5d78f0e0e473d30644094fb8c8a7c99228f2d
Yaroslav Nikonov <ukimbo357@gmail.com>
2026-05-21 16:16:47 +0300

    chore: setup eslint and commit hooks

--- playground/04-objects.js
+++ playground/04-objects.js
@@ -1,27 +1,28 @@
 const user = {
-  name: "Yarik",
+  name: 'Yarik',
   age: 18,
-  email: "Yarik@mail.com",
-  isActive: true
-};
+  email: 'Yarik@mail.com',
+  isActive: true,
+}
+console.log(user)
 const users = [
-    {name: "Vlad", age: 19, email: "Vlad@mail.com", isActive: true},
-    {name: "Vera", age: 30, email: "Vera@mail.com", isActive: false},
-    {name: "Slava", age: 17, email: "Slava@mail.com", isActive: false},
-    {name: "Sasha", age: 25, email: "Sasha@mail.com", isActive: true}
-];
+  { name: 'Vlad', age: 19, email: 'Vlad@mail.com', isActive: true },
+  { name: 'Vera', age: 30, email: 'Vera@mail.com', isActive: false },
+  { name: 'Slava', age: 17, email: 'Slava@mail.com', isActive: false },
+  { name: 'Sasha', age: 25, email: 'Sasha@mail.com', isActive: true },
+]
 
-const isActive = users.filter(user => user.isActive === true);
-console.log(isActive);
+const isActive = users.filter(user => user.isActive === true)
+console.log(isActive)
 
-const userName = isActive.map(user => user.name);
-console.log(userName);
+const userName = isActive.map(user => user.name)
+console.log(userName)
 
-const {name, email} = users[0];
-console.log(`Имя: ${name}, Email: ${email}`);
+const { name, email } = users[0]
+console.log(`Имя: ${name}, Email: ${email}`)
 
 function formatUser({ name, email }) {
-  return `${name} (${email})`;
+  return `${name} (${email})`
 }
-console.log(formatUser(users[0])); 
-console.log(formatUser(users[1])); 
+console.log(formatUser(users[0]))
+console.log(formatUser(users[1]))
