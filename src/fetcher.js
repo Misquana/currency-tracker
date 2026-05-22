c083fd59dcb92ddb30c0d471180b387553a05afd
Yaroslav Nikonov <ukimbo357@gmail.com>
2026-05-22 15:54:18 +0300

    fix(fetcher): added the ! operator before response

--- src/fetcher.js
+++ src/fetcher.js
@@ -3,7 +3,7 @@ const CBR_API_URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
 export async function fetchRawRates() {
   const response = await fetch(CBR_API_URL)
 
-  if (response.ok) {
+  if (!response.ok) {
     throw new Error('CBR API returned status 500')
   }
   const json = await response.json()
